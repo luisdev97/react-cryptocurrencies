3009b1d136ad6fca0603a164bb7283d1ed88c981
luisdev97 <amadormontorodavid@gmail.com>
2019-12-10 20:06:41 +0100

    Show CryptoInfo Component

--- src/components/CryptoInfo.jsx
+++ src/components/CryptoInfo.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 
 const CryptoInfo = ({ cryptoInfo }) => {
     if(Object.keys(cryptoInfo).length === 0 ) return null;
+    
+    const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR , LASTUPDATE } = cryptoInfo;
     return (
-        <div>
-            
+        <div className="resultado">
+            <h2>Result</h2>
+            <p className="precio">The price is <span>{ PRICE }</span></p>
+            <p>Today's highest price <span>{ HIGHDAY }</span></p>
+            <p>Today's lowest price <span>{ LOWDAY }</span></p>
+            <p>Variation in the last 24 hours<span>{ CHANGEPCT24HOUR }</span></p>
+            <p>Actual price <span>{ LOWDAY }</span></p>
         </div>
     );
-};
+}; 
 
-export default CryptoInfo;
+export default CryptoInfo; 
