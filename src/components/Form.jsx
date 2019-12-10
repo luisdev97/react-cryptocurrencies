c21cd18f9036c362cccbdbe8f5339f0dc35cb170
luisdev97 <amadormontorodavid@gmail.com>
2019-12-10 18:47:56 +0100

    Get cryptocurrencies from API

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,19 @@
-import React from 'react';
-
+import React,{ useState, useEffect } from 'react';
+import axios from 'axios';
 const Form = () => {
+
+    const [ cryptocurrencias, setCryptocurrencies ] = useState([]);
+    
+    const getApiInfo = async() => {
+       const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+       const res = await axios.get(url);
+        setCryptocurrencies(res.data.Data);
+    }
+
+    useEffect(() => {
+        getApiInfo();
+    },[]);
+
     return (
         <form>
             <div className="row">
